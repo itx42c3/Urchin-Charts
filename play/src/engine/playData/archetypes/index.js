a485bce6e6ad97c80de4e953e4b3fa4e48c148b3
itx42c3 <itx42c3@gmail.com>
2025-12-26 00:50:01 +0900

    Add TapNote to archetypes export

--- play/src/engine/playData/archetypes/index.js
+++ play/src/engine/playData/archetypes/index.js
@@ -1,7 +1,9 @@
 import { Initialization } from './Initialization.js'
 import { Stage } from './Stage.js'
+import { TapNote } from './TapNote.js' // これを新しく作ります
 
 export const archetypes = defineArchetypes({
     Initialization,
     Stage,
+    TapNote, // 追加
 })
